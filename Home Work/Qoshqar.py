ae5b7e8ae0917d208fd39f00706278eef4066416
qoshqarhuseyn <qoshqarhuseyn1@gmail.com>
2024-03-14 13:51:37 +0400

    commit

--- Home Work/Qoshqar.py
+++ Home Work/Qoshqar.py
@@ -68,6 +68,233 @@
 
 
 
+# Task 1
+
+
+# number = int(input("Number: "))
+# sum = 0
+# for i in range(1, number + 1):
+#     sum += i
+#     print(sum)
+
+
+
+
+
+
+
+
+# Task -2
+
+
+
+
+man= [1,2,3,4,5]
+woman= ["a","b","c"]
+
+gender = int(input)('(kisi, (2) Qadin;)')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Task - 3
+
+
+
+# dict1  = {
+#     "casual": {
+#         "shirt": "t-shirt",
+#         "pants": "jeans",
+#         "shoes": "sneakers"
+#     },
+#     "formal": {
+#         "shirt": "button-up",
+#         "pants": "dress pants",
+#         "shoes": "dress shoes"
+#     },
+#     "sportswear": {
+#         "shirt": "athletic top",
+#         "pants": "athletic shorts",
+#         "shoes": "running shoes"
+#     }
+# }
+
+
+# user_input = input("Enter the type of clothing (casual/formal/sportswear): ")
+
+
+# if user_input in dict1:
+   
+#     print("Shirt:", dict1[user_input]["shirt"])
+#     print("Pants:", dict1[user_input]["pants"])
+#     print("Shoes:", dict1[user_input]["shoes"])
+# else:
+#     print("Invalid input. Please enter either 'casual', 'formal', or 'sportswear'.")
+
+
+
+
+
+
+
+
+# Task - 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Task - 5
+
+
+
+# def bowling():
+#     age = int(input("Please enter your age: "))
+#     if age >= 8:
+#         print("Welcome to the bowling area!")
+#     else:
+#         print("Sorry, those under 8 years old are not allowed in the bowling area.")
+
+# def trampoline():
+#     age = int(input("Please enter your age: "))
+#     weight = int(input("Please enter your weight in kilograms: "))
+#     if age >= 8 and weight < 110:
+#         print("Welcome to the trampoline area!")
+#     else:
+#         print("Sorry, those under 8 years old or over 110 kilograms are not allowed in the trampoline area.")
+
+# def Entertainment():
+#     print("Welcome to the entertainment center!")
+#     sub_category = int(input("Please choose one of the sub-categories: \n1. Dragon \n2. Bouncer \n3. Speed Carousel\n"))
+#     if sub_category == 1:
+#         age = int(input("Please enter your age: "))
+#         weight = int(input("Please enter your weight in kilograms: "))
+#         if age >= 12 and weight < 110:
+#             print("Welcome to the Dragon area!")
+#         else:
+#             print("Sorry, those under 12 years old or over 110 kilograms are not allowed in the Dragon area.")
+#     elif sub_category == 2:
+#         age = int(input("Please enter your age: "))
+#         weight = int(input("Please enter your weight in kilograms: "))
+#         if age >= 6 and weight < 110:
+#             print("Welcome to the Bouncer area!")
+#         else:
+#             print("Sorry, those under 6 years old or over 110 kilograms are not allowed in the Bouncer area.")
+#     elif sub_category == 3:
+#         age = int(input("Please enter your age: "))
+#         height = int(input("Please enter your height in centimeters: "))
+#         weight = int(input("Please enter your weight in kilograms: "))
+#         if age >= 8 and height < 190 and weight < 110:
+#             print("Welcome to the Speed Carousel area!")
+#         else:
+#             print("Sorry, those under 8 years old, over 190 centimeters tall, or over 110 kilograms are not allowed in the Speed Carousel area.")
+
+# def main():
+#     print("Welcome to the entertainment center!")
+#     choice = int(input("Please select the area you want: \n1. Bowling \n2. Trampoline \n3. Entertainment\n"))
+#     if choice == 1:
+#         bowling()
+#     elif choice == 2:
+#         trampoline()
+#     elif choice == 3:
+#         Entertainment()
+#     else:
+#         print("Invalid choice. Please try again.")
+
+# if  "_main_":
+#     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Home Work/Task-3 Qoshqar.py
@@ -0,0 +1,114 @@
+
+
+
+
+# Task - 1
+
+
+# height = float(input("Enter your height in centimeters: "))
+
+
+# ideal_weight_min = (height - 100) * 0.9
+# ideal_weight_max = (height - 100) * 1.1
+
+# print("Recommended ideal weight:", ideal_weight_min, "-", ideal_weight_max, "kilograms")
+
+                                                                           
+
+
+
+
+
+# Task - 2
+
+
+
+# class ShoppingCart:
+#     def _init_(self):
+#         self.items = {}
+#         self.total_price = 0
+
+#     def add_item(self, item_name, item_price):
+#         if item_name in self.items:
+#             self.items[item_name] += item_price
+#         else:
+#             self.items[item_name] = item_price
+#         self.total_price += item_price
+
+#     def calculate_total(self):
+#         total_with_tax = self.total_price * 1.18  
+#         if total_with_tax > 100:
+#             return "You've earned a $15 coupon at the end!"
+#         elif total_with_tax > 50:
+#             return "You've earned a $10 coupon at the end!"
+#         return 
+
+# cart = ShoppingCart()
+
+
+# for _ in range(5):
+#     item_name = input("Enter the name of the item: ")
+#     item_price = float(input("Enter the price of the item (USD): "))
+#     cart.add_item(item_name, item_price)
+
+
+# total_message = cart.calculate_total()
+# print("Total value (including tax): ${:.2f}".format(cart.total_price * 1.18))
+# print(total_message)
+
+
+
+
+
+
+
+
+
+
+
+# Task - 3
+
+
+
+
+
+# def calculate_trip_cost(distance, vehicle_type):
+#     if vehicle_type == "car":
+#         cost_per_km = 0.50
+#     elif vehicle_type == "truck":
+#         cost_per_km = 1.00
+#     elif vehicle_type == "bus":
+#         cost_per_km = 2.00
+#     else:
+#         print("Please enter a valid type of vehicle: car, truck, or bus")
+#         return
+
+#     total_cost = distance * cost_per_km
+#     return total_cost
+
+
+# distance = float(input("Enter the distance of the trip in kilometers: "))
+# vehicle_type = input("Enter the type of vehicle (car, truck, or bus): ")
+
+
+# total_cost = calculate_trip_cost(distance, vehicle_type)
+
+
+# if total_cost is not None:
+#     print("Total cost of the trip: ${:.2f}".format(total_cost))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
